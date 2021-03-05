714cf21fa2b0cd68295e8478a192ec134ac8d9fc
Ivanin Ivanov <ivanin.ivanov@responsiv.co.uk>
2021-03-05 18:13:47 +0000

    [db] expanded db with posts

--- db/r8r-db-build.sql
+++ db/r8r-db-build.sql
@@ -7,6 +7,51 @@ CREATE TABLE users (
         last_login TIMESTAMP 
 );
 
+CREATE TABLE posts (
+	post_id serial PRIMARY KEY,
+	created TIMESTAMP NOT NULL,
+	created_by VARCHAR(50) NOT NULL,
+	type VARCHAR ( 50 ) NOT NULL,
+	title VARCHAR ( 100 ) UNIQUE NOT NULL,
+    description VARCHAR (500)
+);
+
+CREATE TABLE post_images (
+    post_id integer NOT NULL,
+    imageurl text NOT NULL,
+    CONSTRAINT "FK_postimages_post" FOREIGN KEY (post_id)
+        REFERENCES public.posts (post_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
+);
+
+CREATE TABLE post_ratings (
+    post_id integer NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    rating smallint NOT NULL,
+    CONSTRAINT "FK_postratings_post" FOREIGN KEY (post_id)
+        REFERENCES public.posts (post_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID,
+    CONSTRAINT "FK_postratings_user" FOREIGN KEY (username)
+        REFERENCES public.users (username) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
+);
+
+CREATE OR REPLACE VIEW public.avg_post_ratings
+ AS
+SELECT p.*, avg(pr.rating) as 'rating'
+FROM posts AS p
+	LEFT JOIN post_ratings AS pr ON  p.post_id = pr.post_id
+GROUP BY p.post_id;
+
+ALTER TABLE public.avg_post_ratings
+    OWNER TO postgres;
+
 INSERT INTO users (
     username,
     password,
